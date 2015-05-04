b8b5f30f4dc96300c601da21fdb4a84d5afe0d38
Ásgeir Úlfarsson <goling@gmail.com>
2015-05-04 14:10:35 +0000

    testing "is" language

--- src/ios/SpeechToTextModule.m
+++ src/ios/SpeechToTextModule.m
@@ -206,7 +206,8 @@ static void DeriveBufferSize (AudioQueueRef audioQueue, AudioStreamBasicDescript
 
 - (void)postByteData:(NSData *)byteData {
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-    NSString *language = [[NSLocale preferredLanguages] objectAtIndex:0];
+    NSString *language = @"is";
+    NSString *language2 =[[NSLocale preferredLanguages] objectAtIndex:0];
     NSString *urlString = [NSString stringWithFormat:@"https://www.google.com/speech-api/v2/recognize?key=%@&xjerr=1&client=chromium&pfilter=0&lang=%@", consoleApiKey, language];
     NSURL *url = [NSURL URLWithString:urlString];
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
